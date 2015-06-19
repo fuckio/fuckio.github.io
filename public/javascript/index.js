f2b65f16ca70b6ee32e5c45b41f1431339238be6
linan <linan@umeng.com>
2015-06-19 22:09:11 +0800

    init

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -2,4 +2,14 @@
 (function() {
   AV.initialize("ngwh3at1oi9lfqux1so3ie0khzi4s31u4lpz3ry2lg0ekji9",
     "l49a5njmq0tg5uopoq2uu6gzluisue4qssdhg97yrenagl44");
+
+  var TestObject = AV.Object.extend("TestObject");
+  var testObject = new TestObject();
+  testObject.save({
+    foo: "bar"
+  }, {
+    success: function(object) {
+      console.log("LeanCloud works!");
+    }
+  });
 })();
